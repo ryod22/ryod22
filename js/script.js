b10fbbaea2e4d4fa38fe4a069049a3dd24999778
ryod22 <hamzaboy081@gmail.com>
2026-05-14 21:10:01 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -69,9 +69,9 @@ class Particle {
         this.x = Math.random() * canvas.width;
         this.y = Math.random() * canvas.height;
         this.size = Math.random() * 2 + 1;
-        this.speedX = Math.random() * 1 - 0.5;
-        this.speedY = Math.random() * 1 - 0.5;
-        this.opacity = Math.random() * 0.3 + 0.1;
+        this.speedX = Math.random() * 0.5 - 0.25;
+        this.speedY = Math.random() * 2 + 1; // Falling speed (rain-like)
+        this.opacity = Math.random() * 0.4 + 0.2;
     }
     update() {
         // Mouse Repulsion
@@ -89,7 +89,7 @@ class Particle {
         if (this.x < 0 || this.x > canvas.width || this.y < 0 || this.y > canvas.height) this.init();
     }
     draw() {
-        ctx.fillStyle = `rgba(112, 0, 255, ${this.opacity})`;
+        ctx.fillStyle = `rgba(255, 0, 127, ${this.opacity})`; // Pink
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
         ctx.fill();
